73356b8ba70f0bc3cdfed31d472b438b434e3e7b
Alan Ren <10086000+alanvww@users.noreply.github.com>
2024-01-13 18:23:14 -0500

    Project Initiated

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,14 +2,9 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 
-#[tauri::command]
-fn get_secret() -> String {
-  let secret = env!("SECRET");
-  format!("{}", secret)
-}
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![get_environment_variable])
+    .invoke_handler(tauri::generate_handler![])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
